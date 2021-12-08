eb5612a991525d60edc9026fbc2f0dc9d0a98baa
Moussatef <otman.moussetaf@gmail.com>
2021-12-08 09:13:19 +0100

    update forms

---
+++ src/main/java/com/example/demo1/controller/PanelController.java
@@ -0,0 +1,46 @@
+package com.example.demo1.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PanelController {
+    @FXML
+    private Button btnSave;
+    @FXML
+    private TextField inpBadge;
+    @FXML
+    private TextField inpCompany;
+    @FXML
+    private TextField inpAddress;
+    @FXML
+    private TextField inpCin;
+    @FXML
+    private TextField inpFname;
+    @FXML
+    private TextField inpLname;
+    @FXML
+    private TextField inpEmail;
+    @FXML
+    private Label msgBadg ;
+    @FXML
+    private Label msgCompany;
+    @FXML
+    private Label msgWorkstart;
+    @FXML
+    private Label msgAdrress;
+    @FXML
+    private Label msgCin;
+    @FXML
+    private Label msgFname;
+    @FXML
+    private Label msgLname;
+    @FXML
+    private Label msgEmail;
+    @FXML
+    private DatePicker inpDateStart;
+
+
+}
